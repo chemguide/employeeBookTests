33ad0c491acaad2b93382c2234887a40f1fd3621
chemguide <argentum198@gmail.com>
2024-08-19 23:15:47 +0500

    Initial commit

---
+++ src/main/java/com/example/employeeBookTests/EmployeeBookTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeBookTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeeBookTests/EmployeeBookTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeBookTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
